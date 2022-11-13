1f74d47536e7465231beeb223f70630bc1472c86
johnwarejunior <john@johnwarejunior.com>
2022-11-13 10:53:19 -0800

    Still receiving errors. App loads, but doesn't resize images.

--- preload.js
+++ preload.js
@@ -14,9 +14,18 @@ contextBridge.exposeInMainWorld('os', {
   join: (...args) => path.join(...args),
 });
 
-contextBridge.exposeInMainWorld('ipcRenderer', {
-  send: (channel, data) => ipcRenderer.send(channel, data),
-  on: (channel, func) => ipcRenderer.on(channel, (event, ...args) => func(...args))
+// contextBridge.exposeInMainWorld('ipcRenderer', {
+//   send: (channel, data) => ipcRenderer.send(channel, data),
+//   on: (channel, func) => ipcRenderer.on(channel, (event, ...args) => func(...args))
+// });
+
+contextBridge.exposeInMainWorld('electron', {
+  ipcRenderer: {
+    ...ipcRenderer,
+    on: ipcRenderer.on.bind(ipcRenderer),
+    send: ipcRenderer.send.bind(ipcRenderer),
+    removeListener: ipcRenderer.removeListener.bind(ipcRenderer),
+  },
 });
 
 contextBridge.exposeInMainWorld('Toastify', {
